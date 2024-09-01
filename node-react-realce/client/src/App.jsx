837116a4219e2ad064ba6f6c3dd608d201154087
Rudá Mafra <122709872+Ruda-Mafra@users.noreply.github.com>
2024-08-31 23:22:07 -0300

    auth folder, login and register pages created

--- node-react-realce/client/src/App.jsx
+++ node-react-realce/client/src/App.jsx
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
 import Home from "./pages/Home";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/login";
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route exact path="/products/:id" element={<ProductDetail />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
       </Routes>
     </Router>
     </>
